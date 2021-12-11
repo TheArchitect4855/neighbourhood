0953e0e4502f1dd94a2c87bd6a4cdff3ff2da642
Kurtis Knodel <kurtisknodel@gmail.com>
2021-12-11 14:18:30 -0800

    Began homepage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,56 @@
+import Cookies from "cookies";
 import React from "react";
-import Header from "../components/header";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default class Home extends React.Component {
 	render() {
+		const { posts } = this.props;
+		let content = [];
+		if(posts.msg) {
+			content = (
+				<div style={{ textAlign: "center", width: "100%", margin: "1em 0" }}>
+					{ posts.msg }
+				</div>
+			);
+		}
+
 		return (
 			<div>
 				<Header />
+				<main>
+					{content}
+				</main>
+				<Footer />
 			</div>
 		);
 	}
+}
+
+export async function getServerSideProps(ctx) {
+	const { req, res } = ctx;
+	const cookies = new Cookies(req, res);
+
+	const userToken = cookies.get("userToken");
+	if(!userToken) {
+		return {
+			redirect: {
+				destination: "/login",
+				permanent: false,
+			}
+		};
+	}
+
+	// TODO: Validate login token
+
+	// TODO: Get posts
+	const posts = {
+		msg: "No posts yet!",
+	};
+
+	return {
+		props: {
+			posts
+		}
+	};
 }
