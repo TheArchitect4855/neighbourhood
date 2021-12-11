b492b0e3027294bb9e2f881d585007dc29b0691e
Kurtis Knodel <kurtisknodel@gmail.com>
2021-12-11 11:37:10 -0800

    Created custom document

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,7 @@ import Head from 'next/head';
 import '../styles/globals.css'
 
 function Neighbourhood({ Component, pageProps }) {
-  return (
-      <Component {...pageProps}>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        </Head>
-      </Component>
-  );
+  return <Component {...pageProps} />
 }
 
 export default Neighbourhood;
---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class NeighbourhoodDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+					<link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200&display=swap" rel="stylesheet" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
