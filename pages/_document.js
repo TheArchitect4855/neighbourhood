1f1cabdd68435bbecf9c4b5d259822c14f91252a
Kurtis Knodel <kurtisknodel@gmail.com>
2021-12-11 12:38:53 -0800

    Created login page:

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ export default class NeighbourhoodDocument extends Document {
 				<Head>
 					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 					<link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200&display=swap" rel="stylesheet" />
 				</Head>
 				<body>
---
+++ pages/login.js
@@ -0,0 +1,63 @@
+import Footer from "../components/Footer";
+import Head from "next/head";
+import React from "react";
+
+export default class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.sendCode = this.sendCode.bind(this);
+		this.emailForm = React.createRef();
+		this.codeForm = React.createRef();
+	}
+	
+	render() {
+		return (
+			<div>
+				<Head>
+					<title>Log In</title>
+				</Head>
+
+				<header>
+					<h1>Neighbourhood</h1>
+				</header>
+
+				<main>
+					<h2>Log In</h2>
+					<hr />
+
+					<form action="/authenticate" method="POST">
+						<div ref={this.emailForm}>
+							<label htmlFor="email">Email:</label> <br/>
+							<input type="email" name="email" placeholder="email@example.com" required /> <br/>
+							<button type="button" onClick={this.sendCode}>Send Code</button>
+						</div>
+
+						<div ref={this.codeForm} style={{ display: "none" }}>
+							<label htmlFor="code">Code:</label> <br />
+							<input type="text" name="code" placeholder="12345" minLength="5" maxLength="5" required /> <br/>
+							<button type="submit">Log In</button>
+						</div>
+					</form>
+
+					<article>
+						Don&#39;t have an account? Someone needs to invite you to
+						a Neighbourhood in order to create one.
+					</article>
+				</main>
+
+				<Footer />
+			</div>
+		);
+	}
+
+	sendCode(e) {
+		e.preventDefault();
+
+		// TODO: Send user a code
+		const emailForm = this.emailForm.current;
+		emailForm.style.display = "none";
+
+		const codeForm = this.codeForm.current;
+		codeForm.style.display = "block";
+	}
+}
