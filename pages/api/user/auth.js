329862d27f667d0ab5358f3eb75652fbda68c48a
Kurtis Knodel <kurtisknodel@gmail.com>
2021-12-16 10:03:10 -0800

    Rewrote user APIs to use guard clauses

--- pages/api/user/auth.js
+++ pages/api/user/auth.js
@@ -2,37 +2,40 @@ import Cookies from "cookies";
 import { useLoginCode } from "../../../lib/backend";
 
 export default function handler(req, res) {
-	if(req.method == "POST") {
-		const { email, code, remember } = req.body;
-		if(email && code) {
-			const { ok, msg } = useLoginCode(email, code);
-			if(ok) {
-				const options = {};
-				if(remember && remember == "on") {
-					options.maxAge = 30 * 86400000;
-				}
-				
-				const cookies = new Cookies(req, res);
-				cookies.set("userToken", email, options); // TODO: Create actual token
-				res.writeHead(302, {
-					Location: "/"
-				});
-
-				res.end();
-			} else {
-				res.writeHead(302, {
-					Location: `/login?msg=${encodeURIComponent(msg)}`
-				});
-
-				res.end();
-			}
-
-			return;
-		}
+	if(req.method != "POST") {
+		res.status(400).end();
+		return;
 	}
 
+	const { email, code, remember } = req.body;
+	if(!email || !code) {
+		res.writeHead(302, {
+			Location: `/login?msg=${encodeURIComponent("Missing required parameters")}`
+		});
+
+		res.end();
+		return;
+	}
+
+	const { ok, msg } = useLoginCode(email, code);
+	if(!ok) {
+		res.writeHead(302, {
+			Location: `/login?msg=${encodeURIComponent(msg)}`
+		});
+
+		res.end();
+		return;
+	}
+
+	const options = {};
+	if(remember && remember == "on") {
+		options.maxAge = 30 * 86400000;
+	}
+
+	const cookies = new Cookies(req, res);
+	cookies.set("userToken", email, options); // TODO: Create actual token
 	res.writeHead(302, {
-		Location: "/login"
+		Location: "/"
 	});
 
 	res.end();
--- pages/api/user/create.js
+++ pages/api/user/create.js
@@ -2,44 +2,50 @@ import Cookies from "cookies";
 import { createAccount, validateToken } from "../../../lib/backend";
 
 export default function handler(req, res) {
-	if(req.method == "POST") {
-		const cookies = new Cookies(req, res);
-		const userToken = cookies.get("userToken");
-		if(!userToken || !validateToken(userToken)) {
-			const { code } = req.query;
-			if(code) {
-				const { email, nickname, dob, fname, lname, tos } = req.body;
-				if(email && nickname && dob && tos) {
-					const { ok, msg } = createAccount(email, nickname, dob, fname, lname);
-					if(ok) {
-						res.writeHead(302, {
-							Location: `/login?msg=${encodeURIComponent(msg)}`
-						});
+	if(req.method != "POST") {
+		res.status(400).end();
+		return;
+	}
 
-						res.end();
-					} else {
-						res.writeHead(302, {
-							Location: `/invite/${code}?msg=${encodeURIComponent(msg)}`
-						});
+	const cookies = new Cookies(req, res);
+	const userToken = cookies.get("userToken");
+	if(userToken && validateToken(userToken)) {
+		res.writeHead(302, {
+			Location: "/"
+		});
 
-						res.end();
-					}
+		res.end();
+		return;
+	}
 
-					return;
-				} else {
-					res.writeHead(302, {
-						Location: `/invite/${code}?msg=${encodeURIComponent("Missing required parameters")}`
-					});
+	const { code } = req.query;
+	if(!code) {
+		res.status(400).end();
+		return;
+	}
+	 
+	const { email, nickname, dob, fname, lname, tos } = req.body;
+	if(!email || !nickname || !dob || tos != "on") {
+		res.writeHead(302, {
+			Location: `/invite/${code}?msg=${encodeURIComponent("Missing required parameters")}`
+		});
 
-					res.end();
-					return;
-				}
-			}
-		}
+		res.end();
+		return;
+	}
+
+	const { ok, msg } = createAccount(email, nickname, dob, fname, lname);
+	if(!ok) {
+		res.writeHead(302, {
+			Location: `/invite/${code}?msg=${encodeURIComponent(msg)}`
+		});
+
+		res.end();
+		return;
 	}
 
 	res.writeHead(302, {
-		Location: "/"
+		Location: `/login?msg=${encodeURIComponent(msg)}`
 	});
 
 	res.end();
