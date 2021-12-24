49f4d3a7c83b65656d4722b8a7566a666f6a8b66
Kurtis Knodel <kurtisknodel@gmail.com>
2021-12-23 16:28:16 -0800

    Added post commenting

---
+++ pages/api/post/comment.js
@@ -0,0 +1,66 @@
+import Cookies from "cookies";
+import { decode } from "jsonwebtoken";
+import { createComment, getPostData, validateToken } from "../../../lib/backend";
+import Notification from "../../../lib/notifications";
+
+export default async function handler(req, res) {
+	if(req.method != "POST") {
+		res.status(400).end();
+		return;
+	}
+
+	const { comment } = req.body;
+	const { ret } = req.query;
+
+	if(!ret) {
+		res.writeHead(302, {
+			Location: "/",
+		}).end();
+		return;
+	}
+
+	if(!comment) {
+		res.writeHead(302, {
+			Location: `/post/view?id=${ret}&msg=${encodeURIComponent("Missing required parameters")}`,
+		}).end();
+		return;
+	}
+
+	const cookies = new Cookies(req, res);
+	const userToken = cookies.get("userToken");
+	if(!userToken || !validateToken(userToken)) {
+		res.status(403).end();
+		return;
+	}
+
+	const userDataToken = cookies.get("userData");
+	if(!userDataToken || !validateToken(userDataToken)) {
+		res.writeHead(302, {
+			Location: "/api/user/logout"
+		}).end();
+		return;
+	}
+
+	const { uid } = decode(userToken);
+	const { nickname } = decode(userDataToken);
+	try {
+		const { author } = await getPostData(ret);
+		await createComment(uid, ret, comment);
+
+		const notification = Notification.user(
+			`User ${nickname} commented on [your post](/post/view?id=${ret})!`,
+			author
+		);
+
+		notification.send();
+
+		res.writeHead(302, {
+			Location: `/post/view?id=${ret}`
+		}).end();
+	} catch(e) {
+		console.error(e);
+		res.writeHead(302, {
+			Location: `/post/view?id=${ret}&msg=${encodeURIComponent(e.message)}`
+		}).end();
+	}
+}
---
+++ pages/post/view.js
@@ -0,0 +1,143 @@
+import Head from "next/head";
+import React from "react";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Cookies from "cookies";
+import { getCommentsFor, getPostData, validateToken } from "../../lib/backend";
+import * as jwt from "jsonwebtoken";
+import { parseMd } from "../../lib/mdparser";
+import styles from "../../styles/view.module.css";
+
+export default class View extends React.Component {
+	render() {
+		if(!this.props.userData || !this.props.postData) {
+			return (
+				<div>
+					<Head>
+						<title>View Post</title>
+					</Head>
+
+					<Header />
+
+					<main>
+						<article>
+							{ this.props.message }.
+						</article>
+					</main>
+
+					<Footer />
+				</div>
+			)
+		}
+
+		const { uid, rank, nickname } = this.props.userData;
+		const { content, pid } = this.props.postData;
+
+		let comments = [];
+		for(const comment of this.props.comments) {
+			comments.push(
+				<div className={ styles.comment }>
+					<h5>
+						<a href={ `/profile/${comment.author}` } style={{ color: "inherit", textDecoration: "none" }}>{comment.nickname}</a>
+						{ ' ' }
+						<span className="userRank">#{comment.rank}</span>
+					</h5>
+					{ parseMd(comment.content) }
+				</div>
+			);
+		}
+
+		if(comments.length == 0) comments = "No comments yet!";
+
+		return (
+			<div>
+				<Head>
+					<title>View Post</title>
+				</Head>
+
+				<Header />
+
+				<main>
+					<article>
+						<h4>
+							<a href={ `/profile/${uid}` } style={{ color: "inherit", textDecoration: "none" }}>{nickname}</a>
+							{ ' ' }
+							<span className="userRank">#{rank}</span>
+						</h4>
+						{ parseMd(content) }
+					</article>
+
+					<p style={{ color: "red" }}>
+						{ this.props.message }
+					</p>
+
+					<form action={ `/api/post/comment?ret=${pid}` } method="POST" style={{ textAlign: "left" }}>
+						<textarea placeholder="Write a comment..." name="comment" rows="10" cols="40" style={{ resize: "none" }} required></textarea>
+						<br />
+						<button type="submit">Comment</button>
+					</form>
+
+					<article>
+						{ comments }
+					</article>
+				</main>
+
+				<Footer />
+			</div>
+		);
+	}
+}
+
+export async function getServerSideProps(ctx) {
+	const { req, res, query } = ctx;
+	const cookies = new Cookies(req, res);
+	const userToken = cookies.get("userToken");
+	if(!userToken || !validateToken(userToken)) {
+		return {
+			redirect: {
+				destination: "/login",
+				permanent: false,
+			}
+		}
+	}
+
+	let userDataToken = cookies.get("userData");
+	if(!userDataToken || !validateToken(userDataToken)) {
+		try {
+			const { uid } = jwt.decode(userToken);
+			const userData = await getUserData(uid);
+			userDataToken = jwt.sign(userData, "supersecret");
+			cookies.set("userData", userDataToken);
+		} catch(e) {
+			console.error(e);
+			return {
+				redirect: {
+					destination: "/api/user/logout",
+					permanent: false,
+				}
+			}
+		}
+	}
+
+	const { id, msg } = query;
+	const userData = jwt.decode(userDataToken);
+	const postData = await getPostData(id);
+	if(!postData || userData.neighbourhood != postData.neighbourhood) {
+		res.statusCode = 404;
+		return {
+			props: {
+				message: "Post not found",
+			}
+		}
+	}
+
+	const comments = await getCommentsFor(postData.pid);
+	return {
+		props: {
+			userData,
+			postData,
+			comments,
+			message: msg ?? "",
+		}
+	}
+}
